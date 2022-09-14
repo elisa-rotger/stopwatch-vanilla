96da3d79e35028fb9629ed721563ec05a729e190
Elisa Rotger <elisa.rotger@weareadaptive.com>
2022-09-14 12:40:23 +0200

    adding laps to the list

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let timer = document.getElementById('timer');
 let stopButton = document.getElementById('start-stop');
 let lapButton = document.getElementById('lap-reset');
 let isRunning = false;
-let interval;
+let [interval, ms, s, min] = [null, 0, 0, 0];
+let laps = new Array();
 
 stopButton.innerHTML = 'Start';
 lapButton.innerHTML = 'Reset';
@@ -15,11 +16,17 @@ stopButton.addEventListener('click', () => {
 
 lapButton.addEventListener('click', () => {
     if (isRunning) {
-        console.log(interval)
+        // TODO: if timer is running, record laps
+        recordLap();
     } else {
-        // reset: clear interval, set everything to 0, keep clock turned off
+        // RESET: clear interval, set everything to 0, keep clock turned off
         clearInterval(interval);
-        let [minutes, seconds, milliseconds] = [0, 0, 0];
+        [minutes, seconds, milliseconds] = [0, 0, 0];
+        laps = [];
+        let lapsList = document.getElementById('laps-list');
+        lapsList.replaceChildren();
+        let lapNum = document.getElementById('laps-number');
+        lapNum.replaceChildren();
         timer.innerHTML = '00:00.00';
     }
 })
@@ -43,9 +50,9 @@ timerDisplay = () => {
     }
 
     // TODO: figure out how to remove the last 0 in the milliseconds
-    let ms = milliseconds < 100 ? '0' + milliseconds : milliseconds;
-    let s = seconds < 10 ? '0' + seconds : seconds;
-    let min = minutes < 10 ? '0' + minutes : minutes;
+    ms = milliseconds < 100 ? '0' + milliseconds : milliseconds;
+    s = seconds < 10 ? '0' + seconds : seconds;
+    min = minutes < 10 ? '0' + minutes : minutes;
 
     // add these to html
     timer.innerHTML = `${min}:${s}.${ms}`;
@@ -57,4 +64,29 @@ pauseButton = () => {
     stopButton.innerHTML = 'Start';
     stopButton.classList.replace('stop', 'start');
     lapButton.innerHTML = 'Reset';
+}
+
+recordLap = () => {
+    // calc the total of time in the lap
+    let totalTime = min*60*1000 + s*1000 + ms;
+    let value = `${min}:${s}.${ms}`;
+
+    // push obj with: id, value, total to laps array
+    laps.push({ id: laps.length, value: value, total: totalTime});
+
+    let lap = document.createElement('li');
+    lap.innerHTML = value;
+    lap.className = 'lap';
+    let lapsList = document.getElementById('laps-list');
+    lapsList.insertBefore(lap, lapsList.firstChild);
+
+    let num = document.createElement('li');
+    num.innerHTML = `Lap ${laps.length}`;
+    num.className = 'lap-num'
+    let lapNum = document.getElementById('laps-number');
+    lapNum.insertBefore(num, lapNum.firstChild);
+
+    //TODO: Improve styling -> the border boxes for the list elements are already there BEFORE adding the laps, not added with them
+
+    //TODO: Find the lap with highest / lowest value and change their color accordingly
 }
