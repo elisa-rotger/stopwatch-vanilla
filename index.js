ed97264b473b7e57814d72894ee1f7813a4b5b9a
Elisa Rotger <elisa.rotger@elisarotgers-MacBook-Pro.local>
2022-09-14 10:49:20 +0200

    dynamic style for buttons, start / stop functionality

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // initialize time counters
 let [minutes, seconds, milliseconds] = [0, 0, 0];
 let timer = document.getElementById('timer');
+let stopButton = document.getElementById('start-stop');
+let lapButton = document.getElementById('lap-reset');
+let isRunning = false;
+let interval;
+
+stopButton.innerHTML = 'Start';
+lapButton.innerHTML = 'Reset';
 
 // click event on the start / stop button
 // TODO: figure out how to make it stop with the same button
 
-document.getElementById('start-stop').addEventListener('click', () => {
-    // setinterval will run every 10 milliseconds
-    let interval = setInterval(timerDisplay, 10);
+stopButton.addEventListener('click', () => {
+    isRunning ? (pauseButton()) : (interval = setInterval(timerDisplay, 10));
 });
 
 // set up the display of the timer -> increase ms, s and m accordingly
 timerDisplay = () => {
+    isRunning = true;
+    // change inner html and css class of buttons
+    stopButton.innerHTML = 'Stop';
+    stopButton.classList.replace('start', 'stop');
+    lapButton.innerHTML = 'Lap';
+
     milliseconds += 10;
     if (milliseconds === 1000) {
         milliseconds = 0;
@@ -22,9 +34,19 @@ timerDisplay = () => {
         }
     }
 
-    // TODO: set up the display of m, s to be 0 + number when they are < 10
-
+    // TODO: figure out how to remove the last 0 in the milliseconds
+    let ms = milliseconds < 100 ? '0' + milliseconds : milliseconds;
+    let s = seconds < 10 ? '0' + seconds : seconds;
+    let min = minutes < 10 ? '0' + minutes : minutes;
 
     // add these to html
-    timer.innerHTML = `${minutes} : ${seconds} : ${milliseconds}`
+    timer.innerHTML = `${min} : ${s} : ${ms}`
+}
+
+pauseButton = () => {
+    clearInterval(interval);
+    isRunning = false;
+    stopButton.innerHTML = 'Start';
+    stopButton.classList.replace('stop', 'start');
+    lapButton.innerHTML = 'Reset';
 }
