8d88d4622826acc9dbb6ed59048f5869f82613ca
Elisa Rotger <elisa.rotger@weareadaptive.com>
2022-09-16 17:37:01 +0200

    refactoring and renaming variables to conventions

--- index.js
+++ index.js
@@ -1,109 +1,80 @@
-let [milliseconds, seconds, minutes] = [0, 0, 0];
-let [ms, s, min] = [0, 0, 0];
-let [lapMilliseconds, lapSeconds, lapMinutes] = [0, 0, 0];
-let [lapMs, lapSec, lapMin] = [0, 0, 0];
+let milliseconds = 0;
+let lapMilliseconds = 0;
 let interval = null;
 let isRunning = false;
 let laps = new Array();
 
-const timer = document.getElementById('timer');
-const startStopButton = document.getElementById('start-stop');
-const lapResetButton = document.getElementById('lap-reset');
+const $timer = document.getElementById('timer');
+const $startStopButton = document.getElementById('start-stop');
+const $lapResetButton = document.getElementById('lap-reset');
+const $lapList = document.getElementById('lap-list');
 
-const lapList = document.getElementById('lap-list')
+$startStopButton.innerText = 'Start';
+$lapResetButton.innerText = 'Reset';
 
-startStopButton.innerHTML = 'Start';
-lapResetButton.innerHTML = 'Reset';
-
-startStopButton.addEventListener('click', () => {
+$startStopButton.onclick = () => {
     isRunning ? (pauseTimer()) : (interval = setInterval(startTimer, 10));
-});
+};
 
-lapResetButton.addEventListener('click', () => {
-    isRunning ? (recordLap()) : (clearInterval(interval), empty());
-});
+$lapResetButton.onclick = () => {
+    isRunning ? (recordLap()) : (clearInterval(interval), resetTimer());
+};
 
+//TODO: Review setInterval into how it calculates the time (delay?)
 startTimer = () => {
     isRunning = true;
-    startStopButton.innerHTML = 'Stop';
-    startStopButton.classList.replace('start', 'stop');
-    lapResetButton.innerHTML = 'Lap';
+    $startStopButton.innerText = 'Stop';
+    $startStopButton.classList.replace('start', 'stop');
+    $lapResetButton.innerText = 'Lap';
 
-    const runningLap = lapList.firstElementChild;
+    const runningLap = $lapList.firstElementChild;
 
-    // TODO: Refactor this ?
     milliseconds += 10;
-    if (milliseconds === 1000) {
-        milliseconds = 0;
-        seconds += 1;
-        if (seconds === 60) {
-            seconds = 0;
-            minutes += 1;
-        };
-    };
-
     lapMilliseconds += 10;
-    if (lapMilliseconds === 1000) {
-        lapMilliseconds = 0;
-        lapSeconds += 1;
-        if (lapSeconds === 60) {
-            lapSeconds = 0;
-            lapMinutes += 1;
-        };
-    };
 
-    ms = milliseconds === 0 ? '000' 
-        : milliseconds < 100 ? '0' + milliseconds
-        : milliseconds;
-    s = seconds < 10 ? '0' + seconds : seconds;
-    min = minutes < 10 ? '0' + minutes : minutes;
+    let clock = convertToValue(milliseconds);
+    let lapClock = convertToValue(lapMilliseconds);
 
-    lapMs = lapMilliseconds < 100 ? '0' + lapMilliseconds : lapMilliseconds;
-    lapSec = lapSeconds < 10 ? '0' + lapSeconds : lapSeconds;
-    lapMin = lapMinutes < 10 ? '0' + lapMinutes : lapMinutes;
-
-    /* */ 
-
-    let clock = `${min}:${s}.${ms.toString().slice(0, -1)}`;
-    let lapClock = `${lapMin}:${lapSec}.${lapMs.toString().slice(0, -1)}`;
-
-    runningLap.lastElementChild.innerHTML = lapClock;
+    runningLap.lastElementChild.innerText = lapClock;
     runningLap.lastElementChild.classList.remove('empty');
-    runningLap.firstElementChild.innerHTML = `Lap ${laps.length+1}`;
+    runningLap.firstElementChild.innerText = `Lap ${laps.length+1}`;
     runningLap.firstElementChild.classList.remove('empty');
 
-    timer.innerHTML = clock;
+    $timer.innerText = clock;
 }
 
 pauseTimer = () => {
     clearInterval(interval);
     isRunning = false;
 
-    startStopButton.innerHTML = 'Start';
-    startStopButton.classList.replace('stop', 'start');
-    lapResetButton.innerHTML = 'Reset';
+    $startStopButton.innerText = 'Start';
+    $startStopButton.classList.replace('stop', 'start');
+    $lapResetButton.innerText = 'Reset';
 }
 
 recordLap = () => {
 
-    [lapMilliseconds, lapSeconds, lapMinutes] = [0, 0, 0];
-    [lapMs, lapSec, lapMin] = [0, 0, 0];
-
-    let totalTime = (minutes*60*1000) + (seconds*1000) + milliseconds;
-    let value = `${min}:${s}.${ms.toString().slice(0, -1)}`;
+    lapMilliseconds = 0;
 
     if (laps.length === 0) { 
-        laps.push({ value: value, total: totalTime, interval: totalTime });
-        createLap();
+        laps.push({ 
+            value: convertToValue(milliseconds), 
+            total: milliseconds, 
+            interval: milliseconds 
+        });
     } else {
-        let intervalLap = totalTime - laps[laps.length-1].total;
-        value = convertToValue(intervalLap);
-        laps.push({ value: value, total: totalTime, interval: intervalLap });
-        createLap();
-    }
+        let intervalLap = milliseconds - laps[laps.length-1].total;
+        laps.push({ 
+            value: convertToValue(intervalLap), 
+            total: milliseconds, 
+            interval: intervalLap 
+        });
+    };
 
-    let lastLapClasses = lapList.lastElementChild.firstElementChild.classList;
-    lastLapClasses.contains('empty') ? lapList.removeChild(lapList.lastElementChild) : null;
+    createLap();
+
+    let lastLapClasses = $lapList.lastElementChild.firstElementChild.classList;
+    lastLapClasses.contains('empty') ? $lapList.removeChild($lapList.lastElementChild) : null;
 
     if (laps.length >= 3) {
         let edgeLaps = findHighestLowest();
@@ -113,58 +84,59 @@ recordLap = () => {
 }
 
 createLap = () => {
-    const lap = document.createElement('tr');
-    const lapNumber = document.createElement('td');
-    const lapTimer = document.createElement('td');
+    const $lap = document.createElement('tr');
+    const $lapNumber = document.createElement('td');
+    const $lapTimer = document.createElement('td');
 
-    lapTimer.innerHTML = `00:00.0`;
-    lapNumber.innerHTML = `Lap ${laps.length+1}`;
-    lapTimer.className = 'lap';
-    lapNumber.className = 'lap';
+    $lapTimer.innerText = `00:00.0`;
+    $lapNumber.innerText = `Lap ${laps.length+1}`;
+    $lapTimer.id = `${laps.length+1}`;
+    $lapNumber.id = `${laps.length+1}`;
+    $lapTimer.className = 'lap';
+    $lapNumber.className = 'lap';
 
-    lap.appendChild(lapNumber);
-    lap.appendChild(lapTimer);
-    lapList.insertBefore(lap, lapList.firstChild);
+    $lap.appendChild($lapNumber);
+    $lap.appendChild($lapTimer);
+    $lapList.insertBefore($lap, $lapList.firstChild);
 }
 
 convertToValue = (totalMs) => {
-    let minLap = Math.floor(totalMs / 60000);
-    let sLap = Math.floor((totalMs % 60000) / 1000);
-    let msLap = totalMs - (minLap * 60000) - (sLap * 1000);
+    let totalMinutes = Math.floor(totalMs / 60000);
+    let totalSeconds = Math.floor((totalMs % 60000) / 1000);
+    let totalMilliseconds = totalMs - (totalMinutes * 60000) - (totalSeconds * 1000);
 
-    msLap = msLap < 100 ? '0' + msLap : msLap;
-    sLap = sLap < 10 ? '0' + sLap : sLap;
-    minLap = minLap < 10 ? '0' + minLap : minLap;
+    // TODO: Look for built in JS to do this
+    totalMilliseconds = totalMilliseconds === 0 ? '000' : totalMilliseconds < 100 ? '0' + totalMilliseconds : totalMilliseconds;
+    totalSeconds = totalSeconds < 10 ? '0' + totalSeconds : totalSeconds;
+    totalMinutes = totalMinutes < 10 ? '0' + totalMinutes : totalMinutes;
 
-    return `${minLap}:${sLap}.${msLap.toString().slice(0, -1)}`;
+    return `${totalMinutes}:${totalSeconds}.${totalMilliseconds.toString().slice(0, -1)}`;
 }
 
-empty = () => {
-    [milliseconds, seconds, minutes] = [0, 0, 0];
-    [ms, s, min] = [0, 0, 0];
-    [lapMilliseconds, lapSeconds, lapMinutes] = [0, 0, 0];
-    [lapMs, lapSec, lapMin] = [0, 0, 0];
+resetTimer = () => {
+    milliseconds = 0;
+    lapMilliseconds = 0;
     laps = [];
 
-    lapList.replaceChildren();
+    $lapList.replaceChildren();
 
     for (let i = 1; i <= 6; i++) {
         const classes = ['lap', 'empty'];
 
-        const defaultLap = document.createElement('tr');
-        const defaultLapVal = document.createElement('td');
-        const defaultLapNum = document.createElement('td');
+        const $defaultLap = document.createElement('tr');
+        const $defaultLapVal = document.createElement('td');
+        const $defaultLapNum = document.createElement('td');
 
-        defaultLapVal.classList.add(...classes);
-        defaultLapNum.classList.add(...classes);
+        $defaultLapVal.classList.add(...classes);
+        $defaultLapNum.classList.add(...classes);
 
-        defaultLap.appendChild(defaultLapVal);
-        defaultLap.appendChild(defaultLapNum);
+        $defaultLap.appendChild(defaultLapVal);
+        $defaultLap.appendChild(defaultLapNum);
 
-        lapList.appendChild(defaultLap);
+        $lapList.appendChild(defaultLap);
     }
 
-    timer.innerHTML = '00:00.00';
+    $timer.innerText = '00:00.00';
 }
 
 findHighestLowest = () => {
@@ -177,17 +149,14 @@ findHighestLowest = () => {
 }
 
 paintHighestLowest = (highest, lowest) => {
-    const valueLaps = document.querySelectorAll('#lap-list tr td');
-    let [highIdx, lowIdx] = [0, 0];
+    const $valueLaps = document.querySelectorAll('#lap-list tr td');
 
-    valueLaps.forEach((lap, index) => {
+    $valueLaps.forEach(lap => {
         lap.classList.remove('highest', 'lowest');
-        if (lap.innerHTML === highest.value) {
+        if (lap.innerText === highest.value) {
             lap.classList.add('highest');
-            highIdx = index;
-        } else if (lap.innerHTML === lowest.value) {
+        } else if (lap.innerText === lowest.value) {
             lap.classList.add('lowest');
-            lowIdx = index;
         }
     });
 }
@@ -196,3 +165,8 @@ paintHighestLowest = (highest, lowest) => {
 // of the page, and disappear when not being used for a couple of seconds
 
 // TODO: Review styling
+
+// Testing
+$lapList.addEventListener('wheel', () => {
+    console.log('hi!')
+})
